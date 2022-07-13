824c59543603fbd1e2c4ae724bdf0b2b31005f3c
zhengsha <zhengsha@58.com>
2022-07-13 19:43:28 +0800

    feat: 初始化

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <span>{{count}}</span>
+      <button @click="handleClick">点击加1</button>
+      <div>{{result}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      count: 1,
+    };
+  },
+  computed: {
+    result() {
+      return `结果是：${this.count}`;
+    },
+  },
+  watch: {
+    count(val){
+      if(val > 3) {
+        alert(`已经超过${val}啦`);
+      }
+    }
+  },
+  methods: {
+    handleClick() {
+      this.count += 1;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
